743c67bc0ccc7ca8ab8cd5c471d6971f3bf69d04
bartek <bartosz@grandsoftware.pl>
2018-06-29 23:23:04 +0200

    in progress...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,187 @@
+
 window.onload = function () {
-    Particles.init({
-        selector: '.background',
-        color: ['#DA0463', '#404B69', '#DBEDF3'],
-        connectParticles: true
+
+    //Particles initialize
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 40,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": ['#F02651', '#20204E', '#DBEDF3']
+            },
+            "shape": {
+                "type": "image",
+                "stroke": {
+                    "width": 5,
+                    "color": '#222222'
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "assets/img/close-tag.png",
+                    "width": 50,
+                    "height": 50
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 2,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 10,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 50,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 100,
+                "color": "#000",
+                "opacity": 0.5,
+                "width": 2
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 150,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 20,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
     });
+
+    // requestAnimationFrame();
+
+
+
+    // Particles.init({
+    //     selector: '.background',
+    //     color: ['#F02651', '#20204E', '#DBEDF3'],
+    //     connectParticles: true,
+    //     sizeVariations: 10,
+    //     maxParticles: 150,
+    //     speed: 0.5,
+    //     particles: {
+    //         shape: {
+    //             type: "star",
+    //         },
+    //     },
+    //     responsive: [
+    //         {
+    //             breakpoint:
+    //                 768
+    //             ,
+    //             options: {
+    //                 maxParticles:
+    //                     200
+    //                 ,
+    //                 color:
+    //                     '#48F2E3'
+    //                 ,
+    //                 connectParticles:
+    //                     false
+    //             }
+    //         }, {
+    //             breakpoint:
+    //                 425
+    //             ,
+    //             options: {
+    //                 maxParticles:
+    //                     100
+    //                 ,
+    //                 connectParticles:
+    //                     true
+    //             }
+    //         }, {
+    //             breakpoint:
+    //                 320
+    //             ,
+    //             options: {
+    //                 maxParticles:
+    //                     0
+
+    //             }
+    //         }
+    //     ]
+    // });
+
+
+
+
+    //AOS initialize
+    AOS.init();
+
+
+    //Paralax
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
 };
+
+
 
 // set up text to print, each item in array is new line
 var aText = new Array(
--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,22 @@ var browserSync = require('browser-sync').create();
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var mainNodeFiles = require('gulp-main-node-files');
+var stylus = require('gulp-stylus');
 
 
+gulp.task('style-css', function () {
+    return gulp.src('./node_modules/aos/dist/aos.css')
+        .pipe(stylus())
+        .pipe(gulp.dest('./assets/css'));
+});
 
 
-
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('./assets/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
-            outputStyle: 'expanded'
+            outputStyle: 'expanded',
+            includePaths: ['node_modules/aos/dist/aos.css']
         }).on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 5 versions'],
@@ -25,18 +31,20 @@ gulp.task('sass', function() {
 });
 
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
     gulp.src(mainNodeFiles({
         overrides: {
             'codemirror': [
-                'particlesjs/dist/particles.min.js',
+                'particles.js/particles.js',
+                'aos/dist/aos.js',
+                'parallax-js/dist/parallax.js'
             ]
         }
     })).pipe(gulp.dest('./assets/js'));
 })
 
 
-gulp.task('serve', ['sass', 'scripts'], function() {
+gulp.task('serve', ['sass', 'scripts', 'style-css'], function () {
     browserSync.init({
         server: {
             baseDir: "./"
